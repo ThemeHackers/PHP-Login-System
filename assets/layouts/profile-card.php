01dae6bb4634b86b5aef5ab6c595d6ff5c2dc7a1
ds_zct <107047730+ThemeHackers@users.noreply.github.com>
2024-05-18 07:06:02 -0400

    Update profile-card.php

--- assets/layouts/profile-card.php
+++ assets/layouts/profile-card.php
@@ -1,11 +1,8 @@
-
-
-
 <div class='card card-profile text-center box-shadow bg-white'>
 
     <?php if (isset($_SESSION['auth'])) { ?>
 
-    <img alt='' class='card-img-top card-user-cover' src='../assets/images/banner.png'>
+    <img alt='' class='card-img-top card-user-cover' src='../assets/images/cstg.png'>
     <div class='card-block'>
         <a href='../profile'>
             <img src='../assets/uploads/users/<?php echo $_SESSION['profile_image']; ?>' class='card-img-profile'>
@@ -30,18 +27,18 @@
     
     <?php } else { ?>
 
-    <img alt='' class='card-img-top card-user-cover' src='../assets/images/banner.png'>
+    <img alt='' class='card-img-top card-user-cover' src='../assets/images/cstg.png'>
     <div class='card-block'>
-        <a href='https://github.com/msaad1999/PHP-Login-System'>
-            <img src='../assets/images/logo_blackbg.png' class='card-img-profile'>
+        <a href='https://github.com/ThemeHackers/PHP-Login-System'>
+            <img src='../assets/images/lock.jpg' class='card-img-profile'>
         </a>
         <h5  class='card-title'>
             PHP Login System
             <small class="text-muted">
-                <a href="https://github.com/msaad1999">msaad1999@github</a>    
+                <a href="https://github.com/ThemeHackers/">ThemeHackers</a>    
             </small>
             <br>
-            <small class="text-muted">Embeddable PHP Login System</small>
+            <small class="text-muted">Welcome to CSTG Site</small>
         </h5>
     </div>
 
